fe099a4cc3401f7008833db19edb9361107d6377
Theo Ward <theoward9@gmail.com>
2022-04-04 18:03:17 -0500

    employee tests completed, all passing

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -7,15 +7,27 @@ describe('employee', () =>{
             const person = new employee('name', '1', 'name.email');
 
             expect(person.getName()).toBe(`Employee's Name: name`)
-        })
+        });
     });
     describe('getId', () =>{
+        it(`should output the employee's id`, () => {
+            const person = new employee('name', '1', 'name.email');
 
+            expect(person.getId()).toBe(`Employee's Id Number: 1`)
+        });
     });
     describe('getEmail', () =>{
+        it(`should output the employee's email`, () => {
+            const person = new employee('name', '1', 'name.email');
 
+            expect(person.getEmail()).toBe(`Employee's Email Address: name.email`)
+        });
     });
     describe('getRole', () =>{
+        it(`should output the employee's email`, () => {
+            const person = new employee('name', '1', 'name.email');
 
+            expect(person.getRole()).toBe(`Employee's Role: Employee`)
+        });
     });
 })
